d8fad16e6eba9a2414711c8086bcca3a728d7622
Brayan Gamboa <bsgv2005@gmail.com>
2022-03-25 11:02:53 -0500

    25/03/2022

---
+++ data/script.js
@@ -0,0 +1,7 @@
+a
+alert("hola grupo2")
+
+// boton.addEventListener("click",()=>{
+//     const imprimir=document.getElementById('unidadCpu').value
+//     document.getElementById("myDIV2").style.display = "block";
+   
